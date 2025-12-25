b17cec920bf2254fa470c1da8365e231300923c5
Ümit UZ <umituz998@gmail.com>
2025-12-25 06:48:51 +0300

    improvements (2025-12-25 06:48:51)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,20 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: "module",
+    project: "./tsconfig.json",
   },
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
   ],
   rules: {
     "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "warn",
-    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-console": "error",
   },
-  env: {
-    node: true,
-    es2020: true,
-  },
-  ignorePatterns: ["node_modules/", "dist/", "build/", "lib/"],
+  ignorePatterns: ["node_modules/", "*.js"],
 };
+
